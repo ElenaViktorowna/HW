7bd2fbf78a59aa990819510a658d0221d2799a8a
Elena <vorzhieva1989@gmail.com>
2023-02-20 18:24:51 +0300

    completed tasks

--- домашка/task1/Program.cs
+++ домашка/task1/Program.cs
@@ -1,4 +1,6 @@
-﻿int num1, num2, num3;
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int num1, num2, num3;
 Console.Write("Введите первое число: ");
 num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
@@ -14,4 +16,4 @@ if(num3>max)
 {
     max = num3;
 }
-Console.WriteLine("максимальное число: "+max);
+Console.WriteLine("Максимальное число: "+max);
---
+++ домашка/task2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 2.Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int num1, num2;
+Console.Write("Введите первое число: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+            {
+                Console.WriteLine("Первое число " + num1 + " больше чем второе " + num2);
+            }
+            else
+            {
+                Console.WriteLine("Второе число " + num2 + " больше чем первое " + num1);
+            }
