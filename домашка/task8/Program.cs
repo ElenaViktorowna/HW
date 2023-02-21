74f0e4855e7ccc39c17cdc98d3285687c62bb043
Elena <vorzhieva1989@gmail.com>
2023-02-21 19:15:29 +0300

    added task8

---
+++ домашка/task8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 19
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+if(number/10000 == number%10 & number%10000/1000 == number%100/10)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
